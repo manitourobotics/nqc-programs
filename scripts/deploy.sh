2a4fb0a181f1dcff456d65ba759186244c726f63
Azrathud <azrathud@gmail.com>
2012-06-15 12:52:52 -0600

    first init

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# arguments
+for arg in "$@"
+do
+    if [[ "$arg" == "-c" || "$arg" == "--clear" ]]; then
+        # Remove all programs before deploying
+        nqc -clear
+        echo -e "All programs cleared\n"
+    else
+        echo -e "Usage is ./deploy [-c]"
+    fi
+done
+
+for i in {1..5}
+do
+    if [ -e ./$i* ]; then
+        nqc -pgm $i -d $i
+        echo "$i sent"
+    else
+        echo "$i not found"
+    fi
+done
+
